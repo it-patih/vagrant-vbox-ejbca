c2413ed9f9102baaaab22712f05479cdd0764a1f
wahyudi-arifandi <wahyudi-arifandi@user.noreply.github.com>
2023-10-24 00:57:55 +0700

    Fixing ssh remote execution syntax

--- install-ejbca.sh
+++ install-ejbca.sh
@@ -10,5 +10,5 @@ mkdir -p /vagrant/.ejbca
 ssh -q -o StrictHostKeyChecking=no microk8s@localhost << EOF
 	ln -sfn /vagrant/.ejbca /home/microk8s/.ejbca
 	docker pull keyfactor/ejbca-ce
-	docker run -d --name ejbca --rm -p 8080:8080 -p 8443:8443 -h localhost -v /home/microk8s/.ejbca/persistent:/mnt/persistent -e "TLS_SETUP_ENABLED='simple'" -e "DATABASE_JDBC_URL='jdbc:h2:/mnt/persistent/ejbcadb;DB_CLOSE_DELAY=-1'" keyfactor/ejbca-ce
+	docker run -d --name ejbca --rm -p 8080:8080 -p 8443:8443 -h localhost -v /home/microk8s/.ejbca/persistent:/mnt/persistent -e \'TLS_SETUP_ENABLED="simple"\' -e \'DATABASE_JDBC_URL="jdbc:h2:/mnt/persistent/ejbcadb;DB_CLOSE_DELAY=-1"\' keyfactor/ejbca-ce
 EOF
